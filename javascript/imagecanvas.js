5428850874624283084500920d0e037deb2d0858
wjdtjsrms945 <9kjmse@naver.com>
2015-09-15 23:10:10 +0900

    test

--- javascript/imagecanvas.js
+++ javascript/imagecanvas.js
@@ -3,7 +3,8 @@ window.addEventListener("load",drawscreen,true);
 function drawscreen(){
 	var thecanvas=document.getElementById("gamecanvas");
 	var Context=thecanvas.getContext("2d");
-	Context.fillStyle="#000000";
+	var i='F';
+	Context.fillStyle="#ff0000";
 	Context.fillRect(0,0,1000,700);
 	
 }
---
+++ test/event.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",drawscreen,false);
+function drawscreen(){
+	var thecanvas=document.getElementById("gamecanvas");
+	var Context=thecanvas.getContext("2d");
+	var i='F';
+	Context.fillStyle="#ff0000";
+	Context.fillRect(0,0,1000,700);
+	
+}
